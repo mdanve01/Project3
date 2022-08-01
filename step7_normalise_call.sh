f4b5efe27dfa9bc93595dda154e7543e3a2181f0
matthew danvers <109003202+mdanve01@users.noreply.github.com>
2022-08-01 13:12:09 +0100

    Add files via upload

---
+++ step7_normalise_call.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+subject_number=$1
+
+echo $subject_number
+
+### in this case the script folder is same as pwd but in other occasions change the path to your script directory
+
+script_folder=/MRIWork/MRIWork06/nr/matthew_danvers/Study_3/eye_movements_and_rules/script
+
+cd $script_folder
+
+matlab -nosplash -r "step_7_normalise('$subject_number');exit;"
---
+++ step7_normalise_submit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Script to take list of subject names and performs 
+# first level analysis
+
+
+### Select the proper version of matlab
+
+source /usr/local/apps/psycapps/config/matlab_bash
+
+cd /MRIWork/MRIWork06/nr/matthew_danvers/Study_3/eye_movements_and_rules/script/cluster
+
+# Make sure to change this according to your account and that this folder exists
+OUTPUT_LOG_DIR=$(pwd)/log_normalise
+mkdir -p $OUTPUT_LOG_DIR
+
+script_folder=$(pwd)
+
+for subject_number in 301 304 306 309 310 312 313 316 318 319 320 322 323 324 326 328 330 331 333 334 336 340 341 342 401 406 407 410 411 412 413 414 416 418 420 422 423 424 425 426 427 428 429 430 431 432 433 434
+do
+    echo "Processing Subject $subject_number"
+    qsub    -l h_rss=4G \
+            -o ${OUTPUT_LOG_DIR}/matlab_${subject_number}.out \
+            -e ${OUTPUT_LOG_DIR}/matlab_${subject_number}.err \
+            $script_folder/step7_normalise_call.sh $subject_number;       
+done
+
